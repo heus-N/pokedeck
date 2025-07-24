a9fe024c160e0aa02c6cf01c1075558ddbbf5c33
heus <1matheusnuness@gmail.com>
2025-07-24 18:14:47 -0300

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,12 @@
 import React, { Suspense } from 'react';
 import ClientHome from './ClientHome';
-import { useTranslation } from 'react-i18next';
 import { Typography } from '@mui/material';
 
 export default function Home() {
-  const { t } = useTranslation('common');
 
   return (
     <>
-      <Suspense fallback={
-        <div style={{position: 'absolute', margin: 'auto', inset: '0', width: '100vw', height: '100vh', overflow: 'hidden'}}>
-          <Typography variant='h4' color='#fff'>
-            {t('page.loading')}
-          </Typography>
-        </div>}>
+      <Suspense fallback={<></>}>
         <ClientHome />
       </Suspense>
     </>
